b696dc0c998483e231010cc99c805784260f42b0
Cole Paris <coleparis2021@gmail.com>
2022-01-09 18:47:47 -0800

    improved sort tests

--- src/Sorting.java
+++ src/Sorting.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class Sorting {
-    private static final int size = 75000;
+    private static final int size = 100000;
     private static final int range = 1000;
 
     public static void main(String[] args) {
@@ -13,19 +13,54 @@ public class Sorting {
         //System.out.println("unsorted array: "+ Arrays.toString(unsortedBubble));
         bubbleSort(unsortedBubble);
         //System.out.println("bubble sorted array: "+Arrays.toString(unsortedBubble));
+        Integer[] unsortedBubbleDemo = getRandomNumbers();
+        bubbleSortDemo(unsortedBubbleDemo);
 
         //selection sort
-        Integer[] unsortedSelection = getRandomNumbers();
-        //System.out.println("\nunsorted array: "+Arrays.toString(unsortedSelection));
-        Integer[] sortedSelection = selectionSort(unsortedSelection);
-        //System.out.println("selection sorted array: "+Arrays.toString(sortedSelection));
+//        Integer[] unsortedSelection = getRandomNumbers();
+//        //System.out.println("\nunsorted array: "+Arrays.toString(unsortedSelection));
+//        Integer[] sortedSelection = selectionSort(unsortedSelection);
+//        //System.out.println("selection sorted array: "+Arrays.toString(sortedSelection));
+//
+//        //insertion sort
+//        Integer[] unsortedInsertion = getRandomNumbers();
+////        System.out.println("\nunsorted array: "+Arrays.toString(unsortedInsertion));
+//        Integer[] sortedInsertion = insertionSort(unsortedInsertion);
+//        System.out.println("insertion sorted array: "+Arrays.toString(sortedInsertion));
 
 
     }
 
+    private static Integer[] insertionSort(Integer[] array) {
+        int count = 0;
+        System.out.print("insertion sort \n time complexity - best: O(n) worst: O(n^2) \nloading");
+        Instant start = Instant.now();
+
+        for (int i = 1; i < size; ++i) {
+            count++;
+            if(count == size/50) {
+                System.out.print(".");
+                count = 0;
+            }
+
+            Integer next = array[i];
+            Integer currentIndex = i - 1;
+
+            while(currentIndex >= 0 && array[currentIndex] > next){
+                //shift right
+                array[currentIndex+1] = array[currentIndex];
+                currentIndex--;
+            }
+            array[currentIndex+1] = next;
+        }
+        Instant end = Instant.now();
+        Duration timeElapsed = Duration.between(start, end);
+        System.out.println("\nTime Elapsed Insertion Sort: "+timeElapsed.getSeconds()+" seconds\n");
+        return array;
+    }
     private static Integer[] selectionSort(Integer[] unsorted){
         int count = 0;
-        System.out.print("selection sort \nloading");
+        System.out.print("selection sort \n time complexity - best:O(n^2) worst: O(n^2) \nloading");
         Instant start = Instant.now();
         Integer[] sorted = new Integer[size];
         //iterate through unsorted, find minimum
@@ -55,7 +90,7 @@ public class Sorting {
 
     private static void bubbleSort(Integer[] array){
         int count = 0;
-        System.out.print("Bubble sort \nloading");
+        System.out.print("Bubble sort \n time complexity - best: O(n) worst: O(n^2) \nloading");
         Instant start = Instant.now();
         for(int k = 0; k < size; k++){
             count++;
@@ -72,6 +107,36 @@ public class Sorting {
                 }
             }
         }
+
+        Instant end = Instant.now();
+        Duration timeElapsed = Duration.between(start, end);
+        System.out.println("\nTime Elapsed Bubble Sort: "+timeElapsed.getSeconds()+" seconds\n");
+    }
+
+    private static void bubbleSortDemo(Integer arr[]) {
+        int count = 0;
+        System.out.print("Bubble sort demo \n time complexity - best: O(n) worst: O(n^2) \nloading");
+        Instant start = Instant.now();
+
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+
+            count++;
+            if(count == size/50) {
+                System.out.print(".");
+                count = 0;
+            }
+
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap arr[j+1] and arr[j]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
         Instant end = Instant.now();
         Duration timeElapsed = Duration.between(start, end);
         System.out.println("\nTime Elapsed Bubble Sort: "+timeElapsed.getSeconds()+" seconds\n");
